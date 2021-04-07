fdadd671f6aa9028654e10000500533fefdbd395
100piyushsingh <68636197+100piyushsingh@users.noreply.github.com>
2021-04-07 04:05:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,88 +1,14 @@
-import flask
-import difflib
-import numpy as np
-import pandas as pd
-from sklearn.metrics.pairwise import linear_kernel
+from flask import Flask,request,jsonify
+from flask_cors import CORS
+import recommendation
 
-app = flask.Flask(__name__, template_folder='templates')
-
-df = pd.read_csv('model/NetflixFlattened.csv')
-
-# Flattening the dataset
-single_col = ['type', 'rating']
-multi_col = ['director', 'cast', 'country', 'listed_in']
-
-binary_df = pd.DataFrame({'Index':df.index})
-binary_df = binary_df.set_index('Index')
-
- 
-# Single Value
-for i in single_col:
-    for j in df[i].unique():
-        binary_df[j] = 0
-for i in range(len(df)):
-    row = df.index[i]
-    for j in single_col:
-        value = df[j][row]
-        binary_df.loc[row,value] = 1
+app = Flask(__name__)
+CORS(app) 
         
-# Multiple Value
+@app.route('/movie', methods=['GET'])
+def recommend_movies():
+        res = recommendation.results(request.args.get('title'))
+        return jsonify(res)
 
-for i in multi_col:
-    unique_list = []
-    for j in df[i]:
-        for x in j:
-            unique_list.append(x)
-    unique_set = set(unique_list)
-    
-    for value in unique_set:
-        binary_df[value] = 0 
-
-for i in range(len(df)):
-    row = df.index[i]
-    for j in multi_col:
-        for value in df[j][row]:
-            binary_df.loc[row,value] = 1
-
-
-# Compute the cosine similarity matrix
-cosine_sim = linear_kernel(binary_df, binary_df)
-
-# Construct a reverse map of indices and movie titles
-indices = pd.Series(df.index, index=df['title']).drop_duplicates()          
-
-def recommendation_engine(title):
-    
-    title = title.lower()
-    row_index = indices[title]
-    sim_scores = list(enumerate(cosine_sim[row_index]))
-    sim_scores = sorted(sim_scores, key = lambda x: x[1], reverse = True)
-    sim_scores = sim_scores[1:11]
-    movie_indices = [i[0] for i in sim_scores]
-    return df.iloc[movie_indices]['title'].str.upper()
-
-# Set up the main route
-@app.route('/', methods=['GET', 'POST'])
-
-def main():
-    if flask.request.method == 'GET':
-        return(flask.render_template('index.html'))
-            
-    if flask.request.method == 'POST':
-        m_name = flask.request.form['movie_name']
-        m_name = m_name.title()
-#        check = difflib.get_close_matches(m_name,all_titles,cutout=0.50,n=1)
-        if m_name not in all_titles:
-            return(flask.render_template('negative.html',name=m_name))
-        else:
-            result_final = recommendation_engine(m_name)
-            names = []
-            #dates = []
-            for i in range(len(result_final)):
-                names.append(result_final.iloc[i])
-                #dates.append(result_final.iloc[i][1])
-
-            return flask.render_template('positive.html',movie_names=names,movie_date=dates,search_name=m_name)
-
-if __name__ == '__main__':
-    app.run()
+if __name__=='__main__':
+        app.run(port = 5000, debug = True)
