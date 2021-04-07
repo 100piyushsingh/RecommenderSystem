f7cc2af53fc93b343b7463fbb5cb057fef612f7f
100piyushsingh <68636197+100piyushsingh@users.noreply.github.com>
2021-04-07 04:04:55 -0500

    Update recommendation.py

--- recommendation.py
+++ recommendation.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics.pairwise import linear_kernel
 
-df = pd.read_csv('NetflixFlattened.csv')
+df = pd.read_csv('model/NetflixFlattened.csv')
 
 # Flattening the dataset
 single_col = ['type', 'rating']
